6e09e5e825bec791f4daccba7222bb889ddbb180
JOY <32105827+nhjoy@users.noreply.github.com>
2020-10-07 19:00:42 +0800

    Question 1 solved

---
+++ Week 1 Assessment - Nested Data and Iteration/Question 1.py
@@ -0,0 +1,6 @@
+# Question: The variable nested contains a nested list. Assign ‘snake’ to the variable output using indexing.
+
+nested = [['dog', 'cat', 'horse'], ['frog', 'turtle', 'snake', 'gecko'], ['hamster', 'gerbil', 'rat', 'ferret']]
+
+# my code
+output = nested[1][2]
