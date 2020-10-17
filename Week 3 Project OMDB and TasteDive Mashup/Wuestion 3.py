6d3e38489eb52894ce41320f1387f84d5a3d9363
JOY <32105827+nhjoy@users.noreply.github.com>
2020-10-17 19:47:29 +0800

    Question 3 done project

---
+++ Week 3 Project OMDB and TasteDive Mashup/Wuestion 3.py
@@ -0,0 +1,33 @@
+
+# some invocations that we use in the automated tests; uncomment these if you are getting errors and want better error messages
+# get_related_titles(["Black Panther", "Captain Marvel"])
+# get_related_titles([])
+
+import requests_with_caching
+import json
+
+def get_movies_from_tastedive(movie):
+    url = 'https://tastedive.com/api/similar'
+    
+    params= {}
+    params['q'] = movie
+    params['type'] = 'movies'
+    params['limit'] = 5
+    
+    tastedive_resp = requests_with_caching.get(url, params = params)
+
+    return tastedive_resp.json()
+#print(get_movies_from_tastedive("Bridesmaids"))
+
+def extract_movie_titles(data):
+    name = [x['Name'] for x in data['Similar']['Results']]
+    return name
+#print(extract_movie_titles(get_movies_from_tastedive("Bridesmaids")))
+def get_related_titles(movies):
+    lst = []
+    for movie in movies:
+        lst.extend(extract_movie_titles(get_movies_from_tastedive(movie)))
+    return list(set(lst))
+
+
+get_related_titles(["Black Panther", "Captain Marvel"])
